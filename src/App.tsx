1054da2f2d5cc9eaa52d87617e0f6c68cc0bde02
298Ron <298Ron@gmail.com>
2024-05-06 16:47:29 +0300

    LittleChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-
-import { Routes,Route } from "react-router-dom"
-import { Container } from "react-bootstrap"
-import { Home } from "./components/Home"
-import { Navbar } from "./components/Navbar"
-import { Footer } from "./components/Footer"
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home } from "./components/Home";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
 import { ToastContainer } from "react-toastify";
-import {} from "./App.css"
+import {} from "./App.css";
 function App() {
-
-
   return (
-   
     <Container className="mb-4">
-       <ToastContainer />
+      <ToastContainer />
       <Navbar />
-<Routes>
-  <Route path="/" element={<Home/>}/>
-</Routes>
-<Footer/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
